d2407aac0b48caea639957db4df4c54318bce4d0
Konsar <32902921+Konsar@users.noreply.github.com>
2021-10-28 21:05:12 +0200

    Aufgabe 4 Tabellenkalkulation

--- data/4. DOM/tabellenkalkulation.js
+++ data/4. DOM/tabellenkalkulation.js
@@ -1,6 +1,6 @@
 function initialize(){ //Zur Initialisierung der Webseite
     add_eventListener();
-    for(i = 0; i < 3; i++){
+    for(let i = 0; i < 3; i++){
         add_row();
         add_column();
     }
@@ -19,26 +19,9 @@ function add_eventListener(){ //Erzeugt initiale eventListener für + und - Butt
     remove_row_button.addEventListener("click", remove_row);
 }
 
+//Variablen zum Nachhalten der Anzahl der Zeilen und Spalten
 var row_amount = 0;
 var col_amount = 0;
-
-/*          GEDANKENSEKTION
-    IDs für die Zellen vergeben?
-        ->  An Excel angelehnt: Spalten - Buchstaben; Zeilen - Zahlen
-
-    Zellen schreibbar machen
-        ->  Input des Typ Text in die Zelle stecken
-
-    Code auswerten bei verlassen des Focus
-        ->  EventListener (FocusOut?) für Inputs
-
-    Wenn mit = startet, dann Code auswerten
-    Hinterlegte Formel muss dennoch nachgehalten werden
-        ->  Als Attribute hinterlegen
-
-    Bei Klick in eine Zelle mit Formel, formel anzeigen
-        -> EventListener (FocusIn?) für Inputs
-*/
 
 function create_input(){ //Erzeugt einen Input des Typ Text
     var field = document.createElement("input");
@@ -48,8 +31,8 @@ function create_input(){ //Erzeugt einen Input des Typ Text
         if((field.value.toString().startsWith("="))){ //Wenn eine Formel, dann Berechnung starten
             //alert("Formel gefunden");
             field.setAttribute("formula", field.value.toString());
-            calc();
         }
+        calc();
     });
     field.addEventListener("focusin", function(){
         var formula = field.getAttribute("formula");
@@ -64,7 +47,7 @@ function create_cell(id){ //Ezeugt eine Zelle für die ID
     var new_cell = document.createElement("td");
     var field = create_input();
     field.id = id;
-    field.value = id;
+    //field.value = id;
     new_cell.appendChild(field);
     return new_cell;
 }
@@ -83,11 +66,13 @@ function add_column(){ //Fügt Spalte hinzu
     rows[0].insertBefore(header_letter, remove_column_cell); //Fügt den Buchstaben vor dem Button ein
 
     //Zeilen auffüllen
-    for(i = 0; i < row_amount; i++){
+    for(let i = 0; i < row_amount; i++){
         var id = String.fromCharCode(col_amount + 64) + (i + 1);
         var new_cell = create_cell(id);
         rows[i+1].appendChild(new_cell);
     }
+
+    calc();
 }
 
 function remove_column(){ //Entfernt Spalte
@@ -97,7 +82,7 @@ function remove_column(){ //Entfernt Spalte
     //alert(col_amount);
 
     var cells = document.getElementsByTagName("td"); //Greift alle Inhaltszellen ab
-    for(i = 0; i < cells.length; i++){
+    for(let i = 0; i < cells.length; i++){
         children = cells[i].children;
         id = "" + children[0].id
         //alert(id);
@@ -106,6 +91,8 @@ function remove_column(){ //Entfernt Spalte
 
     var remove_col_cell = document.getElementById("remove_col_cell"); //Greift die (tr) Zelle des Löschen Button ab, da vor diesem eingefügt werden muss.
     remove_col_cell.previousSibling.remove(); //Löscht die letzte Zeile
+
+    calc();
 }
 
 function add_row(){ //Fügt Zeile hinzu
@@ -120,7 +107,7 @@ function add_row(){ //Fügt Zeile hinzu
     //alert(header_number.innerHTML);
 
     //Spalten auffüllen
-    for(i = 0; i < col_amount; i++){
+    for(let i = 0; i < col_amount; i++){
         var id = String.fromCharCode(64 + i + 1) + row_amount;
         var new_cell = create_cell(id);
         new_row.appendChild(new_cell);
@@ -128,6 +115,8 @@ function add_row(){ //Fügt Zeile hinzu
 
     var remove_row_cell = document.getElementById("remove_row_cell"); //Greift die (tr) Zelle des Löschen Button ab, da vor diesem eingefügt werden muss.
     remove_row_cell.parentElement.insertBefore(new_row, remove_row_cell); //Fügt neue Zeile vor der Zeile des Löschen Button ein
+
+    calc();
 }
 
 function remove_row(){ //Enternt Zeile
@@ -138,9 +127,96 @@ function remove_row(){ //Enternt Zeile
     var remove_row_cell = document.getElementById("remove_row_cell"); //Greift die (tr) Zelle des Löschen Button ab, da vor diesem eingefügt werden muss.
     remove_row_cell.previousSibling.remove(); //Löscht die letzte Zeile
 
-    //Calc aufrufen, für den Fall, dass eine referenzierte Zelle gelöscht wurde?
+    calc();
 }
 
+//Kalkulationsfunktion
 function calc(){
-    alert("Calculating stuff");
+    //alert("Calculating stuff");
+    var cells = document.getElementsByTagName("input"); //Diesmal nicht die eigentlichen Zellen, sondern deren Input
+    for(let i = 0; i < cells.length; i++){ //Durchlaufe alle Zellen
+        var formula = cells[i].getAttribute("formula"); //Extrahiere Formel
+        formula = formula.replace(" ", ""); //Remove any spaces
+
+        //Warum auch immer werden diese Ausdrücke in JS nicht gematcht
+        //var regex_sum1 = new RegExp("^=SUM\([A-Z]+\d+\,\s?[A-Z]+\d+\)"); // =SUM(A1,B2) oder =SUM(A1, B2)
+        //var regex_sum2 = new RegExp("^=SUM\([A-Z]+\d+\:[A-Z]+\d+\)"); // =SUM(A1:B2)
+
+        var regex_sum = new RegExp("=SUM\(.*\)");
+        if(regex_sum.test(formula)){ //Wenn Summe berechnet werden soll
+            //alert("Regex");
+            var formatted_formula = formula.match(regex_sum)[1]; //Mit dem Pattern matchen und Index 1 nehmen (innere der Klammern)
+            formatted_formula = formatted_formula.replace("(", ""); //Linke Klammer ( löschen
+            formatted_formula = formatted_formula.replace(")", ""); //Rechte Klammer ) löschen
+
+            if(formatted_formula.includes(",")){ //Wenn ,
+                ids = formatted_formula.split(",");
+            } else { //Sonst :
+                ids = formatted_formula.split(":");
+            }
+
+            var x = ids[0];
+            var y = ids[1];
+            cells[i].value = sum(x, y);
+        }
+    }
+}
+
+//Summenfunktion
+function sum(x, y){
+    //alert("SUM");
+    var x_split = splitID(x);
+    var y_split = splitID(y);
+
+    if(x_split[0] > y_split[0]){
+        temp = x_split[0];
+        x_split[0] = y_split[0];
+        y_split[0] = temp;
+    }
+    if(x_split[1] > y_split[1]){
+        temp = x_split[1];
+        x_split[1] = y_split[1];
+        y_split[1] = temp;
+    }
+    //Konvertieren in Integer
+    x_split[0] = x_split[0].charCodeAt(0);
+    y_split[0] = y_split[0].charCodeAt(0);
+    x_split[1] = Number.parseInt(x_split[1]);
+    y_split[1] = Number.parseInt(y_split[1]);
+
+    //Wenn außerhalb der Zellen
+    if(x_split[0] < 64 || y_split[0] - 64 > col_amount) return "Error";
+    else if(x_split[1] < 1 || y_split[1] > row_amount) return "Error";
+
+    //Aufaddieren
+    var sum = 0;
+    for(let i = x_split[0]; i <= y_split[0]; i++){
+        let letter = String.fromCharCode(i);
+        for(let j = x_split[1]; j <= y_split[1]; j++){
+            let id = letter + j;
+            let cell = document.getElementById(id);
+            //Wenn leer, zähl als 0
+            let val = 0;
+            if(cell.value != "") val = Number.parseInt(cell.value);
+
+            sum += val;
+        }
+    }
+    //alert(sum);
+    return sum;
+}
+
+function splitID(id){
+    var id_alphabetic = "";
+    var id_numeric = "";
+    for(let i = 0; i < id.length; i++){
+        if(Number.isInteger(Number.parseInt(id[i]))){
+            id_alphabetic = id.substring(0, i);
+            id_numeric = id.substring(i);
+            break;
+        }
+    }
+    //alert(id_alphabetic + " : " + id_numeric);
+
+    return [id_alphabetic, id_numeric];
 }
