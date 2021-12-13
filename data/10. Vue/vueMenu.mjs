aa749cfc86e92612d2cb5fb7d11c708b9c400d9f
Konsar <32902921+Konsar@users.noreply.github.com>
2021-12-13 14:20:18 +0100

    Aufgabe 10 - Update

--- data/10. Vue/vueMenu.mjs
+++ data/10. Vue/vueMenu.mjs
@@ -3,10 +3,10 @@ import vueMenu from "./vueMenu.mjs";
 
 export default {
     template: `
-        <div style="border: 1px solid black; width: fit-content; height: fit-content">
-            <button>Button 1</button>
-            <button>Button 2</button>
-            <button>Button 3</button>
+        <div style="border: 1px solid black; width: fit-content; height: fit-content;">
+            <button :style="this.$attrs.orientation=='vertical' ? {'display': 'block'}:''" style="margin: 5px;">Button 1</button>
+            <button :style="this.$attrs.orientation=='vertical' ? {'display': 'block'}:''" style="margin: 5px;">Button 2</button>
+            <button :style="this.$attrs.orientation=='vertical' ? {'display': 'block'}:''" style="margin: 5px;">Button 3</button>
         </div>
     `,
 
